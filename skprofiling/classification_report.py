b584d08ebc8724878a8a8ca9a16fe1121a43c3b1
Tiago Alves <tiagohcalves@gmail.com>
2019-10-05 11:01:56 -0300

    Add more plots

--- skprofiling/classification_report.py
+++ skprofiling/classification_report.py
@@ -1,14 +1,15 @@
-from typing import List, Optional
-from itertools import cycle
 import warnings
+from inspect import signature
+from itertools import cycle
+from typing import List, Optional
 
-from IPython import display
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from IPython import display
 from matplotlib import pyplot as plt
 from scipy import interp
-from sklearn import metrics
+from sklearn import metrics, calibration
 
 from .utils import get_max_score, display_markdown
 
@@ -18,24 +19,29 @@ FIGSIZE_X = 10
 FIGSIZE_Y = 8
 
 
-def plot_pr_curve(y_true: List[float], y_pred: List[float]) -> None:
-    pass
-
-
-def plot_f1_th(y_true: List[float], y_pred: List[float]) -> None:
-    pass
-
-
-def plot_acc_th(y_true: List[float], y_pred: List[float]) -> None:
-    pass
-
-
-def plot_confusion_matrix(y_true: List[float], y_pred: List[float]) -> None:
-    pass
-
-
 def plot_calibration(y_true: List[float], y_pred: List[float]) -> None:
-    pass
+    display_markdown("## Probability Calibration Curve")
+
+    clf_score = metrics.brier_score_loss(y_true, y_pred, pos_label=y_true.max())
+    fraction_of_positives, mean_predicted_value = calibration.calibration_curve(y_true, y_pred, n_bins=10)
+
+    fig, ax = plt.subplots(2, 1, figsize=(FIGSIZE_X, FIGSIZE_Y))
+
+    ax[0].plot([0, 1], [0, 1], "k:", label="Perfectly calibrated")
+    ax[0].plot(mean_predicted_value, fraction_of_positives, "s-", label=f"Brier Score: {clf_score:0.2f}")
+    ax[1].hist(y_pred, range=(0, 1), bins=10, histtype="step", lw=2)
+
+    ax[0].set_ylabel("Fraction of positives")
+    ax[0].set_ylim([-0.05, 1.05])
+    ax[0].legend(loc="lower right")
+    ax[0].set_title('Calibration plots (reliability curve)')
+
+    ax[1].set_xlabel("Mean predicted value")
+    ax[1].set_ylabel("Count")
+    # ax[1].legend(loc="upper center", ncol=2)
+
+    plt.tight_layout()
+    plt.show()
 
 
 def plot_metrics_over_time(y_true: List[float], y_pred: List[float], date_index: List) -> None:
@@ -45,15 +51,14 @@ def plot_metrics_over_time(y_true: List[float], y_pred: List[float], date_index:
 def classification_report(y_true: List[float], y_pred: List[float], date_index: Optional[list] = None) -> None:
     print_static_metrics(y_true, y_pred)
 
+    display_markdown("---")
     display_markdown("# Plots")
     display_markdown("The plots shown here are meant to give insights about the distributions of the model")
 
     dist_score_plot(y_true, y_pred)
     plot_roc_curve(y_true, y_pred)
     plot_pr_curve(y_true, y_pred)
-    plot_f1_th(y_true, y_pred)
-    plot_acc_th(y_true, y_pred)
-    plot_confusion_matrix(y_true, y_pred)
+    plot_th_impact(y_true, y_pred)
     plot_calibration(y_true, y_pred)
 
     if date_index is not None:
@@ -69,10 +74,10 @@ def print_static_metrics(y_true: List[float], y_pred: List[float]) -> None:
     )
 
     metrics_dict = {
-        "Area Under ROC Curve": [metrics.roc_auc_score(y_true, y_pred), np.nan],
+        "Area Under ROC Curve*": [metrics.roc_auc_score(y_true, y_pred), np.nan],
         "F1-Score": get_max_score(metrics.f1_score, y_true, y_pred),
         "Accuracy": get_max_score(metrics.accuracy_score, y_true, y_pred),
-        "Average Precision-Recall": [metrics.average_precision_score(y_true, y_pred), np.nan],
+        "Average Precision-Recall*": [metrics.average_precision_score(y_true, y_pred), np.nan],
         "Jaccard Score": get_max_score(metrics.jaccard_score, y_true, y_pred),
         "Humming Loss": get_max_score(metrics.accuracy_score, y_true, y_pred)
     }
@@ -83,7 +88,9 @@ def print_static_metrics(y_true: List[float], y_pred: List[float]) -> None:
 
     display_markdown("<br>")
     print(metrics.classification_report(y_true, np.round(y_pred)))
-    display_markdown("---")
+
+    display_markdown("<br>")
+    display_markdown("\\* These metrics do not depend on the threshold.")
 
 
 def dist_score_plot(y_true: List[float], y_pred: List[float]) -> None:
@@ -126,12 +133,20 @@ def compute_roc(y_pred, y_true):
 
 def plot_roc_curve(y_true, y_pred):
     display_markdown("## ROC Curve")
+    display_markdown("This curve indicates how many false positive one should expect before encountering a true "
+                     "positive.")
+    display_markdown("Usually, the bigger the area under the ROC curve, the better the model is predicting the target"
+                     "class. ")
+    display_markdown("A random model would have a straight line plot.")
+    display_markdown("Also, the shape of the curve indicates if the model is making more mistakes on false positives"
+                     "or false negatives.")
 
     if y_true.ndim == 1:
         roc_auc, fpr, tpr = compute_roc(y_pred.reshape(-1, 1), y_true.reshape(-1, 1))
     else:
         roc_auc, fpr, tpr = compute_roc(y_pred, y_true)
 
+    # From https://scikit-learn.org/stable/auto_examples/model_selection/plot_roc.html
     n_classes = len(np.unique(y_true)) - 1
 
     # Compute macro-average ROC curve and ROC area
@@ -153,7 +168,7 @@ def plot_roc_curve(y_true, y_pred):
     roc_auc["macro"] = metrics.auc(fpr["macro"], tpr["macro"])
 
     # Plot all ROC curves
-    plt.figure()
+    plt.figure(figsize=(FIGSIZE_X, FIGSIZE_Y))
     plt.plot(fpr["micro"], tpr["micro"],
              label='micro-average ROC curve (area = {0:0.2f})'
                    ''.format(roc_auc["micro"]),
@@ -177,4 +192,53 @@ def plot_roc_curve(y_true, y_pred):
     plt.ylabel('True Positive Rate')
     plt.title('Receiver Operating Characteristic')
     plt.legend(loc="lower right")
+    plt.show()
+
+
+def plot_pr_curve(y_true: List[float], y_pred: List[float]) -> None:
+    display_markdown("## PR Curve")
+    display_markdown("This plot shows the trade-off between precision and recall for different thresholds.")
+    display_markdown("The bigger the area under the PR curve, the better the model is predicting the target"
+                     "class. ")
+
+    # From https://scikit-learn.org/stable/auto_examples/model_selection/plot_precision_recall.html
+    average_precision = metrics.average_precision_score(y_true, y_pred)
+    precision, recall, _ = metrics.precision_recall_curve(y_true, y_pred)
+
+    step_kwargs = ({'step': 'post'}
+                   if 'step' in signature(plt.fill_between).parameters
+                   else {})
+
+    plt.figure(figsize=(FIGSIZE_X, FIGSIZE_Y))
+    plt.step(recall, precision, color='b', alpha=0.2, where='post')
+    plt.fill_between(recall, precision, alpha=0.2, color='b', **step_kwargs)
+
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('Precision-Recall curve. Average Precision = {0:0.2f}'.format(average_precision))
+    plt.show()
+
+
+def plot_th_impact(y_true: List[float], y_pred: List[float]) -> None:
+    display_markdown("## Threshold Impact")
+    display_markdown("This plot shows how the threshold impacts the model's performance")
+    display_markdown("It indicates regions of threshold where the model is better separating the classes.")
+    display_markdown("It should be useful when one is considering a sub-optimum threshold to meet some system"
+                     "requirement.")
+
+    f1_score, max_th, score_list_f1, thresholds = get_max_score(metrics.f1_score, y_true, y_pred, with_values=True)
+    acc_score, max_th, score_list_acc, thresholds = get_max_score(metrics.accuracy_score, y_true, y_pred, with_values=True)
+
+    plt.figure(figsize=(FIGSIZE_X, FIGSIZE_Y))
+    plt.plot(thresholds, score_list_f1, label="F1-Score")
+    plt.plot(thresholds, score_list_acc, label="Accuracy")
+
+    plt.xlabel('Threshold')
+    plt.ylabel('Metric Value')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('Metrics by Threshold. Best F1-Score = {0:0.2f}'.format(f1_score))
+    plt.legend()
     plt.show()
